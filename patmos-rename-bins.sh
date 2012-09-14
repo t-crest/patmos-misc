9bdfe1e09b103b942f3d48aca1f2c4dbbe6a2534
Daniel Prokesch <daniel.prokesch@gmail.com>
2012-09-14 13:40:33 +0200

    added script to prefix binaries with 'patmos-'

---
+++ patmos-rename-bins.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Rename executables in specified dir to have 'patmos-' prefix
+# and recreate local links to the renamed executables
+#
+# Most probably you want to do that in your Patmos <install_dir>/bin
+# if you did not use the build.sh script
+#
+# Daniel Prokesch <daniel@vmars.tuwien.ac.at>
+#
+
+DIR=${1:-.}
+
+if [ ! -d $DIR ]; then
+  echo "Error: Directory '"$DIR"' does not exist!" 1>&2
+  exit 1
+fi
+
+if [[ ! $(cd $DIR ; pwd) =~ /bin$ ]]; then
+  echo "Error: Directory does not end in '/bin'!" 1>&2
+  exit 1
+fi
+
+# following function only works for local links to local targets within $DIR
+function _sc_relink {
+  local bname="${1##*/}"
+  if [[ ! $bname =~ ^patmos- ]]; then
+    echo "Relink $bname" 1>&2
+    ln -s -T "patmos-$(readlink $1)" "${1%/*}/patmos-$bname"
+    rm $1
+  else
+    echo "Skip $bname" 1>&2
+  fi
+}
+
+function _sc_rename {
+  local bname="${1##*/}"
+  if [[ ! $bname =~ ^patmos- ]]; then
+    echo "Rename $bname" 1>&2
+    mv "$1" "${1%/*}/patmos-$bname"
+  else
+    echo "Skip $bname" 1>&2
+  fi
+}
+
+export -f _sc_relink _sc_rename
+
+find -P $DIR -type l -executable -exec bash -c '_sc_relink {}' \;
+find -P $DIR -type f -executable -exec bash -c '_sc_rename {}' \;
