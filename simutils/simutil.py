772a733c04bf62e7fe5a79d2d0b6b84d0b3ca130
Daniel Prokesch <daniel.prokesch@gmail.com>
2012-12-09 23:29:27 +0100

    simutils: trace_ex

--- simutils/simutil.py
+++ simutils/simutil.py
@@ -40,6 +40,31 @@ def trace(binary):
 
 ###############################################################################
 
+def trace_ex(binary):
+  """Generator for extended execution trace in EX stage (cyc, instr addresses + PRR before)"""
+  pasim_cmd = ['pasim', '--debug=0', '--debug-fmt=default', binary]
+  # send stdout to /dev/null
+  with open(os.devnull, 'w') as fnull:
+    pasim = Popen(pasim_cmd, stderr=PIPE, stdout=fnull)
+    ro = re.compile(r'^.*PRR: ([0-9]{8}) .* PC : 0*([0-9a-fA-F]{1,8})') # regex object
+    pipeline = [None, None] # initial pipline fill
+    cyc = 0
+    for line in pasim.stderr:
+      mo = ro.match(line) # matcher object
+      if mo:
+        (prr, inst) = mo.groups()
+        pipeline.append(inst)
+        yield cyc, prr, pipeline.pop(0)
+        cyc = cyc + 1
+    # drain pipelne
+    yield cyc,   prr, pipeline.pop(0)
+    yield cyc+1, prr, pipeline.pop(0)
+    # processed each line, now wait
+    ret = pasim.wait()
+    if ret: raise SimError(ret)
+
+###############################################################################
+
 
 if __name__=='__main__':
   raise Exception("This module is not to be executed.")
