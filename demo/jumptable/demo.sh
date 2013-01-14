4fd057d4f43bf8109d8a1b58816abd937fd54fe0
Stefan Hepp <stefan@stefant.org>
2013-01-14 15:11:18 +0100

    updated path

--- demo/jumptable/demo.sh
+++ demo/jumptable/demo.sh
@@ -8,6 +8,6 @@ set -x
 
 patmos-clang -o jumptable -mpatmos-serialize=jumptable.pml -mpatmos-serialize-skip-instructions jumptable.c 
 #../patmos-llvm/tools/psk/psk-merge -o jumptable.pml jumptable.pml
-../patmos-llvm/tools/psk/psk-pml2ais -g -a jumptable.apx -b jumptable -r report.log -x results.xml -e measure -o jumptable.ais jumptable.pml
+../../../patmos-llvm/tools/psk/psk-pml2ais -g -a jumptable.apx -b jumptable -r report.log -x results.xml -e measure -o jumptable.ais jumptable.pml
 pasim -q jumptable
 a3patmos -b jumptable.apx
