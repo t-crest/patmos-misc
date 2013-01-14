3054e8f42f7a6f013a9edffb6a10e02ed1db6c18
Stefan Hepp <stefan@stefant.org>
2013-01-14 16:24:06 +0100

    Updated demo script arguments, ignore temp demo files

--- demo/jumptable/demo.sh
+++ demo/jumptable/demo.sh
@@ -6,8 +6,8 @@ fi
 
 set -x
 
-patmos-clang -o jumptable -mpatmos-serialize=jumptable.pml -mpatmos-serialize-skip-instructions jumptable.c 
+patmos-clang -o jumptable -mpatmos-serialize=jumptable.pml jumptable.c 
 #../patmos-llvm/tools/psk/psk-merge -o jumptable.pml jumptable.pml
-../../../patmos-llvm/tools/psk/psk-pml2ais -g -a jumptable.apx -b jumptable -r report.log -x results.xml -e measure -o jumptable.ais jumptable.pml
+../../../patmos-llvm/tools/psk/psk-pml2ais -g -a jumptable.apx -b jumptable -r report.log -x results.xml -e measure --ais jumptable.ais jumptable.pml
 pasim -q jumptable
 a3patmos -b jumptable.apx
