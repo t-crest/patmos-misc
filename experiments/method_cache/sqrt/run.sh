656a175fb916252a14cfbfb9fb51feb96948b2cb
Stefan Hepp <stefan@stefant.org>
2015-08-17 11:01:47 +0200

    Fixed -v option in sqrt test script

--- experiments/method_cache/sqrt/run.sh
+++ experiments/method_cache/sqrt/run.sh
@@ -8,7 +8,7 @@ for opt in "" "--mcmethods 32" "-m 2k" "-C icache -K dm" "-C icache -K lru2" "-C
   echo "** pasim $opt:"
   for i in sqrt sqrt.nosc sqrt.ps512 sqrt.nosc.ps512 sqrt.ps768 sqrt.nosc.ps768 sqrt.nops sqrt.nosc.nops; do 
     if [ "$1" == "-v" ]; then
-      echo "pasim -v $PASIM_OPTS $opt $i"
+      echo "pasim -v $PASIM_OPTS $opt $i" >&2
     fi
     echo -n "$i "
     pasim -v $PASIM_OPTS `echo $opt` $i 2>&1 | grep "Cycles:"
@@ -17,8 +17,14 @@ done
 
 for opt in "" "-M fifo32" "-m 2k" "-M fifo32 -D ideal" "-C icache -M dm" "-C icache -M lru2" "-C icache -M lru4" "-C icache -M lru8"; do
   echo "** platin $opt:"
+  if [ "$1" == "-v" ]; then
+    echo "platin pml-config -o config.pml --target patmos-unknown-unknown-elf $opt" >&2
+  fi
   platin pml-config -o config.pml --target patmos-unknown-unknown-elf `echo $opt`
   for i in sqrt sqrt.nosc sqrt.ps512 sqrt.nosc.ps512 sqrt.ps768 sqrt.nosc.ps768 sqrt.nops sqrt.nosc.nops; do 
+    if [ "$1" == "-v" ]; then
+      echo "platin wcet -b $i -i ${i}.pml -i config.pml $PLATIN_OPTS --report report.txt" >&2
+    fi
     echo -n "$i "
     platin wcet -b $i -i ${i}.pml -i config.pml $PLATIN_OPTS --report report.txt 2>/dev/null
     sim=`cat report.txt | grep -A 1 trace | grep cycles | sed "s/ *cycles: *//"`
