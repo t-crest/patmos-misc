07feae204e7e14dd72f6355e4220bbe26cee2b4e
Martin Schoeberl <martin@jopdesign.com>
2015-12-03 11:42:31 -0800

    test: start with regression tests

---
+++ regtest-init.sh
@@ -0,0 +1,10 @@
+# do a clean build and test of T-CREST 
+export PATH=$HOME/t-crest-test/local/bin:$PATH
+rm -rf $HOME/t-crest-test
+mkdir $HOME/t-crest-test
+cd $HOME/t-crest-test
+git clone git@github.com:t-crest/patmos-misc.git misc > result.txt
+# rest is done in regtest
+./misc/regtest.sh 2>> result.txt
+RECIPIENTS=`cat $HOME/t-crest-test/misc/recipients.txt`
+mail -s "[T-CREST] Build report `date`" ${RECIPIENTS} < $HOME/t-crest-test/result.txt
---
+++ regtest.sh
@@ -0,0 +1,6 @@
+# do a clean build and test of T-CREST
+# this is called from regtest-init.sh
+./misc/build.sh
+# cd patmos
+# make test
+./misc/build.sh -t
