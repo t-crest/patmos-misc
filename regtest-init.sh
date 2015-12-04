e6516a02d9d24984e1971ba99b053eca15e5289a
Martin Schoeberl <martin@jopdesign.com>
2015-12-04 00:49:19 -0800

    test: minor tweak

--- regtest-init.sh
+++ regtest-init.sh
@@ -3,8 +3,8 @@ export PATH=$HOME/t-crest-test/local/bin:$PATH
 rm -rf $HOME/t-crest-test
 mkdir $HOME/t-crest-test
 cd $HOME/t-crest-test
-git clone git@github.com:t-crest/patmos-misc.git misc > result.txt
+git clone git@github.com:t-crest/patmos-misc.git misc
 # rest is done in regtest
-./misc/regtest.sh &>> result.txt
+./misc/regtest.sh &> result.txt
 RECIPIENTS=`cat $HOME/t-crest-test/misc/recipients.txt`
 mail -s "[T-CREST] Build report `date`" ${RECIPIENTS} < $HOME/t-crest-test/result.txt
