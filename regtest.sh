b4bff88ee65c54c6712fa7e071f722e50fb2a148
Martin Schoeberl <martin@jopdesign.com>
2015-12-04 00:42:35 -0800

    test: another variation

--- regtest.sh
+++ regtest.sh
@@ -1,6 +1,10 @@
 # do a clean build and test of T-CREST
 # this is called from regtest-init.sh
 ./misc/build.sh
-# cd patmos
-# make test
+# this test is not supported from build.sh
+cd patmos
+make test
+# first do a bench build
+./misc/build.sh bench
+./misc/build.sh -t bench
 ./misc/build.sh -t
