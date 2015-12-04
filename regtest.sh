3a8e1836893636609b65bc15534e5727ff657c6f
Martin Schoeberl <martin@jopdesign.com>
2015-12-04 12:07:19 -0800

    test: another iteration

--- regtest.sh
+++ regtest.sh
@@ -1,11 +1,22 @@
 # do a clean build and test of T-CREST
 # this is called from regtest-init.sh
+# Do individual tests as test all does not work
 ./misc/build.sh
 # this test is not supported from build.sh
 cd patmos
 make test
 cd ..
-# first do a bench build
-./misc/build.sh bench
+# this is now the simulator test
+./misc/build.sh -t patmos
 ./misc/build.sh -t bench
-./misc/build.sh -t
+##./misc/build.sh -t gold # no tests
+##./misc/build.sh -t llvm # fails always
+##./misc/build.sh -t newlib # no tests
+##./misc/build.sh -t compiler-rt # no tests
+# RTEMS does not build on my (MS) machine - needs some more checks on dependencies
+##./misc/build.sh -t rtems 
+##./misc/build.sh -t rtems-test
+##./misc/build.sh -t rtems-examples
+##./misc/build.sh -t eclipse # this is a alias to llvm
+##./misc/build.sh -t aegean # no tests
+##./misc/build.sh -t poseidon # no tests
