59f45ae97a69541f5ccae7a233e2ccc71f3def22
Martin Schoeberl <martin@jopdesign.com>
2015-12-14 11:39:06 -0800

    test: regression test shall not use the developer version of the repos

--- regtest-init.sh
+++ regtest-init.sh
@@ -3,7 +3,7 @@ export PATH=$HOME/t-crest-test/local/bin:$PATH
 rm -rf $HOME/t-crest-test
 mkdir $HOME/t-crest-test
 cd $HOME/t-crest-test
-git clone git@github.com:t-crest/patmos-misc.git misc
+git clone https://github.com/t-crest/patmos-misc.git misc
 # rest is done in regtest
 echo Testing T-CREST > result.txt
 ./misc/regtest.sh
