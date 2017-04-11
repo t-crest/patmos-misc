6b6f98a98a153f3a5d79c4f35e5a5d956bfb2f1c
Daniel Prokesch <daniel.prokesch@gmail.com>
2017-04-11 10:37:48 +0200

    cache_prefetch_lock/wcet2017: unified Makefile
    * hopefully solved the sed issue
    * target doit can be removed once we agree it works

--- experiments/cache_prefetch_lock/wcet2017/modmain.sh
+++ experiments/cache_prefetch_lock/wcet2017/modmain.sh
@@ -1,2 +1,3 @@
-find . -name "*.c" | xargs sed -i "" '/\(int\|void\)\s\+main/i\'$'\n''__attribute__((noinline))'
+#!/bin/bash
+find . -name "*.c" -exec sed -i'.orig' '/\(int\|void\)\s\+main/i\'$'\n''__attribute__((noinline))' '{}' \+
 #find . -name "*.c" | xargs sed -i "" 's/.*[^_]main.*;$/int main( void ) __attribute__ ((noinline));/'
