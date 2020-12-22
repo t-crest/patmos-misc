4bfb42ef3be8f97752dc7babfdd30f14412cae1b
Martin Schoeberl <martin@jopdesign.com>
2020-12-22 16:31:33 +0100

    tests: debug Helena

--- regtest.sh
+++ regtest.sh
@@ -4,10 +4,10 @@
 
 # Do individual tests as test all does not work
 # To debug on Helena, put all output into the result (just now):
-./misc/build.sh &>> ../result.txt
+./misc/build.sh >> ../result.txt
 # this test is not supported from build.sh
 cd patmos
-make test &>> ../result.txt
+make test >> ../result.txt
 cd ..
 # this is now the simulator test
 ./misc/build.sh -t simulator >> result.txt
